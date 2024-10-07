a8a3726e94b023516512ef49371b7dc11253990c
Mattias Axelsson <mattiasaxelsson@icloud.com>
2024-10-07 23:43:44 +0200

    deleted index.css and added banner logo, border and basic menu

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,11 +2,13 @@ import './styles/SideBar.css'
 function SideBar() {
   return (
     <div className="category-sidebar">
-      <button>Action</button>
-      <button>Drama</button>
-      <button>Comedy</button>
-      <button>Horror</button>
-      <button>Sci-Fi</button>
+      <ul>
+        <li><a href="#">Action</a></li>
+        <li><a href="#">Drama</a></li>
+        <li><a href="#">Comed</a></li>
+        <li><a href="#">Horror</a></li>
+        <li><a href="#">Sci-Fi</a></li>
+        </ul>
     </div>
   );
 }
